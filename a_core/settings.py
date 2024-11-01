974453b87ec1fd4f9ddaa8aee6d8add1be400c05
malripo <malripo741205@gmail.com>
2024-11-01 14:43:26 +0900

    Herobutton

--- a_core/settings.py
+++ a_core/settings.py
@@ -7,6 +7,10 @@ env = Env()
 Env.read_env()
 ENVIRONMENT = env('ENVIRONMENT', default='production')
 
+# Feature Toggle
+DEVELOPER = env('DEVELOPER', default='')
+# STAGING = env('STAGING', default=False)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -64,6 +68,7 @@ INSTALLED_APPS = [
         
     'a_posts',
     'a_users',
+    'a_features',    
 ]
 
 # for django-allauth
